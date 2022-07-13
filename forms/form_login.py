af7a87c9db85de622814f626fbaf57b75ab0e8e7
AlbertoPerezRguez <albertoperezguez@gmail.com>
2022-07-13 16:58:56 -0500

    Edicion de persistencia

--- forms/form_login.py
+++ forms/form_login.py
@@ -5,6 +5,21 @@ import util.generic as utl
 from forms.form_master import MasterPanel
 
 class App:
+
+
+    def verificar(self):
+        usu = self.usuario.get()
+        password = self.password.get()
+        if(usu =="root" and password == "1234"):
+            self.ventana.destroy()
+            MasterPanel()
+        else:
+            messagebox.showerror(message="La contraseña no es correcta", title="Mensaje")
+
+
+
+
+
     def __init__(self):
          self.ventana = tk.Tk()
          self.ventana.title('Inicio de sesion')
@@ -35,11 +50,44 @@ class App:
 
          #frame_form_top
          #Encabezado del login
-         frame_form_top = tk.Frame(frame_form,height = 50, bd=0, relief=tk.SOLID, bg='black')
+         frame_form_top = tk.Frame(frame_form,height = 90, bd=0, relief=tk.SOLID, bg='black')
          frame_form_top.pack(side="top", fill = tk.X)
-         title = tk.Label(frame_form_top, text="SISTEMA DE CONSULTA", font=('Times', 30), fg="#666a88", bg='#fcfcfc', pady=50)
+         title = tk.Label(frame_form_top, text="SISTEMA DE CONSULTA", font=('Times', 30), fg="#666a88", bg='#fcfcfc', pady=20)
          title.pack(expand=tk.YES,fill=tk.BOTH)
          #frame_for_top
+
+        #frame_form_top
+         #Encabezado 2
+         frame_form_top2 = tk.Frame(frame_form,height = 10, bd=0, relief=tk.SOLID, bg='black')
+         frame_form_top2.pack(side="top", fill = tk.X)
+         title = tk.Label(frame_form_top2, text="INICIO DE SESION", font=('Times', 20), fg="#666a88", bg='#fcfcfc', pady=40)
+         title.pack(expand=tk.YES,fill=tk.BOTH)
+         #frame_for_top
+
+         #Frame de rellenado
+         frame_form_fill = tk.Frame(frame_form,height = 50, bd=0, relief=tk.SOLID,bg='#fcfcfc')
+         frame_form_fill.pack(side="bottom", expand=tk.YES,fill=tk.BOTH)
+
+
+         etiqueta_usuario = tk.Label(frame_form_fill, text="Usuario", font=('Times', 14), fg="#666a88", bg='#fcfcfc',anchor="w")
+         etiqueta_usuario.pack(fill=tk.X,padx=20,pady=5)
+         self.usuario = ttk.Entry(frame_form_fill, font=('Times', 14))
+         self.usuario.pack(fill=tk.X,padx=20,pady=10)
+
+
+         etiqueta_password = tk.Label(frame_form_fill, text="Contraseña", font=('Times', 14), fg="#666a88", bg='#fcfcfc',anchor="w")
+         etiqueta_password.pack(fill=tk.X,padx=20,pady=5)
+         self.password = ttk.Entry(frame_form_fill, font=('Times', 14))
+         self.password.pack(fill=tk.X,padx=20,pady=10)
+         self.password.config(show="*")
+
+
+         inicio= tk.Button(frame_form_fill,text="Iniciar sesion", font=('Times', 15,BOLD), bg='#3a7ff6', bd =0,fg="#fff",command=self.verificar)
+         inicio.pack(fill=tk.X, padx=20,pady=20)
+         inicio.bind("<Return>", (lambda event: self.verificar()))  
+
+         #Frame de rellenado
+
 
 
          self.ventana.mainloop()
